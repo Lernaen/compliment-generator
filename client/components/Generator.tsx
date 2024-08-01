dc3759e4524cf2f92b2aa6eebef82f6734281bbd
Jem Halpine <jemhalpine@gmail.com>
2024-08-02 11:04:31 +1200

    start generator

--- client/components/Generator.tsx
+++ client/components/Generator.tsx
@@ -2,7 +2,11 @@ import compliments from './Compliments'
 
 import { useState } from 'react'
 
-const [compliment, setCompliment] = useState
-//Function to get random number to use in the compliments array
+function generateCompliment(){
+ const [compliment, setCompliment] = useState
+//Add function to get random number to use in the compliments array
+const randomAdjective = adjectives[(Math.floor(Math.random() * adjectives.length))]
 
+const randomNoun = 
 //Return HTML element with selected compliment
+}
