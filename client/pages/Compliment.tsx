50ee8dcdf9d041b6cff8a6508360d1aaff8580e7
Rebecca-Lang <125948480+Rebecca-Llang@users.noreply.github.com>
2024-08-02 14:43:18 +1200

    unfinished handleClick

--- client/pages/Compliment.tsx
+++ client/pages/Compliment.tsx
@@ -1,5 +1,17 @@
 import { generateCompliment } from '../components/Generator'
+import { useState } from 'react'
 
-export default function Compliment() {
-  return <>{generateCompliment()}</>
+export default function newCompliment() {
+  const [compliment, setCompliment] = useState('')
+
+  const handleClick = () => {
+    setCompliment(generateCompliment())
+  }
+
+  return (
+    <div>
+      <p>{compliment}</p>
+      <button onClick={handleClick}>Generate Compliment</button>
+    </div>
+  )
 }
--- client/pages/Home.tsx
+++ client/pages/Home.tsx
@@ -1,12 +1,16 @@
+import { generateCompliment } from '../components/Generator'
+import newCompliment from './Compliment'
+
 export default function Home() {
   return (
     <>
       {' '}
       <main>
         <div className="complimentBox"></div>
+
+        <button className="complimentBox">{generateCompliment()}</button>
+        <button className="genAgain ">Again, Again</button>
       </main>
-      <button className="complimentBox">You are... like... </button>
-      <button className="genAgain ">Again, Again</button>
     </>
   )
 }
